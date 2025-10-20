05d555ef84677f73b87239ab4e7030105650a7c3
Andrew <106273840@student.swin.edu.au>
2025-10-20 22:27:20 +1100

    Create .php file for settings

---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+    $host = "locahost"; // Connects to the local Apache Web Server (XAMPP).
+    $username = "root";
+    $password = ""; // No password set. Designated by assignment requirements.
+    $database = "eoi"; // "Expression of Interest" table
+?>
