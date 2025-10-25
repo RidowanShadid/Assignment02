2229a7fd1255f0d2009d62b787f7e84ac0b2c58e
Andrew <106273840@student.swin.edu.au>
2025-10-25 16:15:55 +1100

    Add error message when redirected from login page

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -18,6 +19,12 @@
 
         <!-- ================= MAIN CONTENT ================= -->
         <main>
+            <?php
+                if(isset($_SESSION['error'])) {
+                    echo "<p style='color:red;'>", $_SESSION['error'], $_SESSION['username'], "<p>";
+                    unset($_SESSION['error']);
+                } 
+            ?>
             <!-- Intro section -->
             <h2>What Is Innovexa Labs?</h2>
             <section>
