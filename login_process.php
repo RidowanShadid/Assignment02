d11f8d1da0af040fc37c9a3f2ae409305efd3b2a
Andrew <106273840@student.swin.edu.au>
2025-10-25 16:18:16 +1100

    Close connection to database on query completion

--- login_process.php
+++ login_process.php
@@ -3,13 +3,15 @@
 session_start();
 // Database connection details from settings.php
 require_once("settings.php");
+
 // Connecting to database - error if connection failed
 $dbcon = mysqli_connect($host, $username, $password, $database);
 if(!$dbcon) {
     die("Connection to database unsuccessful." . mysqli_connect_error());
 }
+
 // Checking for proper form submission
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
+if(($_SERVER['REQUEST_METHOD'] == 'POST') && !empty($_POST['username']) && !empty($_POST['password'])) {
     // Get the input from the login form, and remove whitespaces
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
@@ -18,13 +20,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $result = mysqli_query($dbcon, $query);
     if(($user = mysqli_fetch_assoc($result)) && (password_verify($password, $user['password']))) {
         // Check if query was successful and entered password matches hashed password
-        // Set session variable as username and redirect to manager page
+        // Set session variable as username, closes mysql connection, and redirect to manager page
         $_SESSION['username'] = $user['username'];
+        mysqli_close($dbcon);
         header('Location: manage.php');
         exit();
     } else {
-        // Login error if query returns nothing at all or there is an invalid username/password - sends back to login page
+        // Login error if query returns nothing at all or there is an invalid username/password - closes sql connection and sends back to login page
         $_SESSION['error'] = "Please try again (Invalid username and/or password).";
+         mysqli_close($dbcon);
         header('Location: login.php');
         exit();
     }
